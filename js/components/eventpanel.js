7870ecb3dbf7fad8204fb82eb78ec8935c9f361b
pkillthetoy <tonyzhao099@gmail.com>
2020-10-22 22:17:52 -0700

    Create a simple event panel template

--- js/components/eventpanel.js
+++ js/components/eventpanel.js
@@ -1,10 +1,18 @@
 app.component('eventpanel', {
+    data() { return {
+      eventtext: "Sample event text goes here",
+      buttontexts: ["Button 1", "Button 2"],
+      choice: "None"
+    }},
     props: [
         'bartype',
         'level'
     ],
     computed: {
     },
-    template: `
-    >`
+    template: `<div>{{eventtext}}</div>
+    <div>{{choice}}</div>
+    <span v-for="text in buttontexts">
+      <button v-on:click="choice=text">{{text}}</button>
+    </span>`
 })
